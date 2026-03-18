da301ca767fba707e051337c28000267e5a3afd1
novatel-applications-engineering <support.novatel@hexagon.com>
2026-03-18 14:04:51 -0600

    Imported upstream version '24.2.1' of 'upstream'

--- src/novatel_oem7_driver/src/oem7_receiver_file.cpp
+++ src/novatel_oem7_driver/src/oem7_receiver_file.cpp
@@ -24,9 +24,7 @@
 
 #include <novatel_oem7_driver/oem7_receiver_if.hpp>
 
-#include <ros/ros.h>
 
-#include <boost/asio.hpp>
 #include <fstream>
 
 
@@ -37,9 +35,11 @@ namespace novatel_oem7_driver
    */
   class Oem7ReceiverFile: public Oem7ReceiverIf
   {
+    rclcpp::Node* node_;
+
     std::ifstream oem7_file_; ///< Input
 
-    size_t num_byte_read_; ///< Total number of bytes read from file.
+    unsigned int num_byte_read_; ///< Total number of bytes read from file.
 
 
   public:
@@ -55,18 +55,20 @@ namespace novatel_oem7_driver
     /**
      * Prepares the input file for reading. The file is opened as binary/read only.
      */
-    virtual bool initialize(ros::NodeHandle& nh)
+    virtual bool initialize(rclcpp::Node& nh)
     {
-      std::string oem7_file_name;;
-      nh.getParam("oem7_file_name", oem7_file_name);
+      node_ = &nh;
 
-      ROS_INFO_STREAM("Oem7File['" << oem7_file_name << "']");
+      node_->declare_parameter("oem7_file_name", "");
+      std::string oem7_file_name = node_->get_parameter("oem7_file_name").as_string();
+
+      RCLCPP_INFO_STREAM(node_->get_logger(), "Oem7File['" << oem7_file_name << "']");
 
       oem7_file_.open(oem7_file_name, std::ios::in | std::ios::binary);
       int errno_value = errno; // Cache errno locally, in case any ROS calls /macros affect it.
       if(!oem7_file_)
       {
-        ROS_ERROR_STREAM("Could not open '" << oem7_file_name << "'; error= " << errno_value << " '"
+        RCLCPP_ERROR_STREAM(node_->get_logger(), "Could not open '" << oem7_file_name << "'; error= " << errno_value << " '"
                                             << strerror(errno_value) << "'");
         return false;
       }
@@ -78,11 +80,11 @@ namespace novatel_oem7_driver
      * Reads input from file.
      *
      */
-    virtual bool read( boost::asio::mutable_buffer buf, size_t& rlen)
+    virtual bool read(unsigned char* data, unsigned int data_len, unsigned int& rlen)
     {
       if(!oem7_file_)
       {
-        ROS_ERROR_STREAM("Error accessing file." );
+        RCLCPP_ERROR_STREAM(node_->get_logger(), "Error accessing file." );
         return false;
       }
 
@@ -94,7 +96,7 @@ namespace novatel_oem7_driver
         sleep(3); // Use absolute sleep, as this is not related to ROS internal timing.
       }
 
-      oem7_file_.read(boost::asio::buffer_cast<char*>(buf), boost::asio::buffer_size(buf));
+      oem7_file_.read(reinterpret_cast<char*>(data), data_len);
       int errno_value = errno; // Cache errno locally, in case any ROS calls /macros affect it.
 
       rlen = oem7_file_.gcount();
@@ -102,14 +104,14 @@ namespace novatel_oem7_driver
 
       if(oem7_file_.eof())
       {
-        ROS_INFO_STREAM("No more input available. Read " << num_byte_read_ << " bytes." );
+        RCLCPP_ERROR_STREAM(node_->get_logger(), "No more input available. Read " << num_byte_read_ << " bytes." );
 
         return false;
       }
 
       if(!oem7_file_)
       {
-        ROS_ERROR_STREAM("Error " << errno_value << " reading input: '"  << strerror(errno_value) << "'" );
+        RCLCPP_ERROR_STREAM(node_->get_logger(), "Error " << errno_value << " reading input: '"  << strerror(errno_value) << "'" );
 
         return false;
       }
@@ -122,12 +124,13 @@ namespace novatel_oem7_driver
      *
      * @return false always.
      */
-    virtual bool write(boost::asio::const_buffer buf)
+
+    virtual bool write(const unsigned char* data, const unsigned int data_len)
     {
       return false;
     }
   };
 }
 
-#include <pluginlib/class_list_macros.h>
+#include <pluginlib/class_list_macros.hpp>
 PLUGINLIB_EXPORT_CLASS(novatel_oem7_driver::Oem7ReceiverFile, novatel_oem7_driver::Oem7ReceiverIf)
